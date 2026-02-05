eaa972a54a80d5003a4e57a73d27ff2203c9bee8
ajay kumar <116097393+ajay8473@users.noreply.github.com>
2026-02-05 21:10:47 +0530

    Initial commit - hello-api-next

---
+++ app/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json(
+    {
+      message: "Hello API is working "
+    },
+    { status: 200 }
+  );
+}
